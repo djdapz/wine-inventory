281a4aeb6e197160202baed97f53eba02fb48fbe
djdapz <djdapz@aol.com>
2018-11-08 17:48:00 -0600

    Add db connection info to enviornment for backend tests.

--- ci/tasks/run-backend-tests-exe.sh
+++ ci/tasks/run-backend-tests-exe.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+export DB_URL="jdbc:postgresql://test-db:5432/test?user=user&password=pwd"
+export DB_USERNAME=user
+export DB_PASSWORD=pwd
+
 pushd repo
     ./gradlew clean test
 popd
