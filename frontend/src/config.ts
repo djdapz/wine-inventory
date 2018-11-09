7e8ce7f1b1d9c98761a553582bdc556650f2bdbf
djdapz <djdapz@aol.com>
2018-11-08 20:55:49 -0600

    add prod config

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,8 @@
 enum EnvironmentName {
     DEV = "DEV",
     E2E = "E2E",
-    DEVELOPMENT_PCF="DEVELOPMENT_PCF"
+    DEVELOPMENT_PCF="DEVELOPMENT_PCF",
+    PROD="PROD",
 }
 
 const environment: EnvironmentName = function () {
@@ -12,6 +13,8 @@ const environment: EnvironmentName = function () {
             return EnvironmentName.E2E;
         case("https://wine-inventory-acceptance.cfapps.io"):
             return EnvironmentName.DEVELOPMENT_PCF;
+        case("https://wine-inventory.cfapps.io"):
+            return EnvironmentName.PROD;
         default:
             return EnvironmentName.E2E
     }
@@ -21,7 +24,8 @@ const environment: EnvironmentName = function () {
 const backendUriMap = {
     [EnvironmentName.DEV]: "http://localhost:8081",
     [EnvironmentName.E2E]: "http://localhost:3210",
-    [EnvironmentName.DEVELOPMENT_PCF]: "https://wine-inventory-backend-acceptance.cfapps.io"
+    [EnvironmentName.DEVELOPMENT_PCF]: "https://wine-inventory-backend-acceptance.cfapps.io",
+    [EnvironmentName.PROD]: "https://wine-inventory-backend-production.cfapps.io"
 };
 
 export const backendUri = backendUriMap[environment];
