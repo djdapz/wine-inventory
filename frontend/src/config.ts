8fd03cec1360efa42b143a811c6e6ef6579b0ac8
djdapz <djdapz@aol.com>
2018-11-08 19:51:08 -0600

    add backend deploy step

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,6 +1,7 @@
 enum EnvironmentName {
     DEV = "DEV",
-    E2E = "E2E"
+    E2E = "E2E",
+    DEVELOPMENT_PCF="DEVELOPMENT_PCF"
 }
 
 const environment: EnvironmentName = function () {
@@ -9,6 +10,8 @@ const environment: EnvironmentName = function () {
             return EnvironmentName.DEV;
         case("http://localhost:3210"):
             return EnvironmentName.E2E;
+        case("https://wine-inventory-acceptance.cfapps.io"):
+            return EnvironmentName.DEVELOPMENT_PCF;
         default:
             return EnvironmentName.E2E
     }
@@ -17,7 +20,8 @@ const environment: EnvironmentName = function () {
 
 const backendUriMap = {
     [EnvironmentName.DEV]: "http://localhost:8081",
-    [EnvironmentName.E2E]: "http://localhost:3210"
+    [EnvironmentName.E2E]: "http://localhost:3210",
+    [EnvironmentName.DEVELOPMENT_PCF]: "https://wine-inventory-backend-acceptance.cfapps.io"
 };
 
 export const backendUri = backendUriMap[environment];
