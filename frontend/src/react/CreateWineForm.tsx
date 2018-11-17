5b7d1c3203a0c363d0bf51622fbc736268d56879
djdapz <djdapz@aol.com>
2018-11-16 18:28:22 -0600

    Use drawer for wine form.

--- frontend/src/react/CreateWineForm.tsx
+++ frontend/src/react/CreateWineForm.tsx
@@ -8,12 +8,12 @@ import {
     addProducerToWineRequest,
     addQuantityToWineRequest,
     addTypeToWineRequest,
-    addYearToWineRequest, closeCreateWineForm,
+    addYearToWineRequest,
+    closeCreateWineForm,
     openCreateWineForm,
     submitCreateWine
 } from "../redux/CreateWine.actions";
 import Button from "@material-ui/core/Button/Button";
-import styled from "styled-components";
 import Cancel from "@material-ui/icons/Cancel";
 import {BottomButton} from "./NewWineButton";
 import Select from "@material-ui/core/Select/Select";
@@ -23,6 +23,8 @@ import {CreateWineRequest, isWineRequestReadyToSubmit} from "../domain/CreateWin
 import {Country} from "../redux/Country.actions";
 import {StoreType} from "../index";
 import FilledInput from "@material-ui/core/es/FilledInput/FilledInput";
+import styled from "styled-components";
+import Drawer from "@material-ui/core/Drawer/Drawer";
 
 interface CreateWineFormProps {
     createWineFormRequest: CreateWineRequest | null
@@ -47,19 +49,13 @@ let handleNum = (fun: (n?: number) => void) => (event: React.ChangeEvent<HTMLInp
     }
 };
 
-interface Collapsable {
-    isCollapsed: boolean
-}
-
-const StyledForm = styled.div<Collapsable>`
+const StyledForm = styled.div`
   display: flex;
   flex-direction: column;
   background-image: linear-gradient(to bottom right,#8e2dfa,rgb(160, 44, 157));
-  padding: ${p => p.isCollapsed ? 0 : "1rem"};
-  position: fixed;
-  right: 0;
+  padding: 1rem;
   height: 100%;
-  max-width: ${p => p.isCollapsed ? 0 : "10rem"};
+  max-width:10rem;
   input{
     background-color: white;
     border-top-left-radius: 4px;
@@ -71,85 +67,89 @@ const StyledForm = styled.div<Collapsable>`
     border-top-right-radius: 4px;
   }
 `;
+
 const renderMenuItems = (countries: Country[]) => countries.map(country =>
     <option key={country.name}
             value={country.name}>{country.name}</option>);
 
-const CreateWineForm = (props: CreateWineFormProps) =>
-    props.createWineFormRequest ?
-        <StyledForm id="create-wine-form"
-                    isCollapsed={false}>
-            <BottomButton>
-                <Button
-                    id={"cancel-new-wine"}
-                    variant={'fab'}
-                    color={"secondary"}
-                    onClick={props.closeForm}>
-                    <Cancel/>
-                </Button>
-            </BottomButton>
 
-            <FormControl variant="filled"
-                         className={'country-input'}>
-                <InputLabel htmlFor="country-dropdown">Country</InputLabel>
-                <Select
-                    native
-                    value={props.createWineFormRequest.country}
-                    onChange={(event) => props.changeCountry(event.target.value)}
-                    input={<FilledInput name="country"
-                                        id="country-dropdown"/>}
-                >
-                    <option value=""/>
-                    {renderMenuItems(props.countries)}
-                </Select>
-            </FormControl>
-            <TextField
-                className={'producer-input'}
-                label="Producer"
-                value={props.createWineFormRequest.producer}
-                onChange={(event) => props.changeProducer(event.target.value)}
-                margin="normal"
-                variant="filled"
-            />
-            <TextField
-                className={'type-input'}
-                label="Type"
-                value={props.createWineFormRequest.type}
-                onChange={(event) => props.changeType(event.target.value)}
-                margin="normal"
-                variant="filled"
-            />
-            <TextField
-                className={'year-input'}
-                label="Year"
-                value={props.createWineFormRequest.year}
-                onChange={handleNum(props.changeYear)}
-                margin="normal"
-                variant="filled"
-            />
-            <TextField
-                className={'quantity-input'}
-                label="Quantity"
-                value={props.createWineFormRequest.quantity}
-                onChange={handleNum(props.changeQuantity)}
-                margin="normal"
-                variant="filled"/>
-            <TextField
-                className={'cellar-location-input'}
-                label="Cellar Location"
-                value={props.createWineFormRequest.cellarLocation}
-                onChange={(event) => props.changeCellarLocation(event.target.value)}
-                margin="normal"
-                variant="filled"/>
-            <Button disabled={!isWineRequestReadyToSubmit(props.createWineFormRequest)}
-                    className={"submit-button"}
-                    color={"secondary"}
-                    variant={'contained'}
-                    onClick={() => props.submit(props.createWineFormRequest!)}>
-                Create
-            </Button>
-        </StyledForm>
-        : <div/>;
+const CreateWineForm = (props: CreateWineFormProps) =>
+    <Drawer
+        anchor="right"
+        open={props.createWineFormRequest !== null && props.createWineFormRequest !== undefined}
+        onClose={() => null}>
+        {props.createWineFormRequest ?
+            <StyledForm>
+                <FormControl variant="filled"
+                             className={'country-input'}>
+                    <InputLabel htmlFor="country-dropdown">Country</InputLabel>
+                    <Select
+                        native
+                        value={props.createWineFormRequest.country}
+                        onChange={(event) => props.changeCountry(event.target.value)}
+                        input={<FilledInput name="country"
+                                            id="country-dropdown"/>}
+                    >
+                        <option value=""/>
+                        {renderMenuItems(props.countries)}
+                    </Select>
+                </FormControl>
+                <TextField
+                    className={'producer-input'}
+                    label="Producer"
+                    value={props.createWineFormRequest.producer}
+                    onChange={(event) => props.changeProducer(event.target.value)}
+                    margin="normal"
+                    variant="filled"
+                />
+                <TextField
+                    className={'type-input'}
+                    label="Type"
+                    value={props.createWineFormRequest.type}
+                    onChange={(event) => props.changeType(event.target.value)}
+                    margin="normal"
+                    variant="filled"
+                />
+                <TextField
+                    className={'year-input'}
+                    label="Year"
+                    value={props.createWineFormRequest.year}
+                    onChange={handleNum(props.changeYear)}
+                    margin="normal"
+                    variant="filled"
+                />
+                <TextField
+                    className={'quantity-input'}
+                    label="Quantity"
+                    value={props.createWineFormRequest.quantity}
+                    onChange={handleNum(props.changeQuantity)}
+                    margin="normal"
+                    variant="filled"/>
+                <TextField
+                    className={'cellar-location-input'}
+                    label="Cellar Location"
+                    value={props.createWineFormRequest.cellarLocation}
+                    onChange={(event) => props.changeCellarLocation(event.target.value)}
+                    margin="normal"
+                    variant="filled"/>
+                <Button disabled={!isWineRequestReadyToSubmit(props.createWineFormRequest)}
+                        className={"submit-button"}
+                        color={"secondary"}
+                        variant={'contained'}
+                        onClick={() => props.submit(props.createWineFormRequest!)}>
+                    Create
+                </Button>
+                <BottomButton>
+                    <Button
+                        id={"cancel-new-wine"}
+                        variant={'fab'}
+                        color={"secondary"}
+                        onClick={props.closeForm}>
+                        <Cancel/>
+                    </Button>
+                </BottomButton>
+            </StyledForm> : <StyledForm/>}
+    </Drawer>;
 
 const mapStateToProps = (state: StoreType) => ({
     createWineFormRequest: state.createWineForm,
